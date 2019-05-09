d3116d50a391dc5eb3a04a5a14c08a1aa5c5a9ac
zeotuan <zeotuan@gmail.com>
2019-05-09 20:12:34 +1000

    install cors header run this before runserver:pip install django-cors-headers

--- server/TimShop/TimShop/settings.py
+++ server/TimShop/TimShop/settings.py
@@ -57,7 +57,13 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
 ]
+CORS_ORIGIN_ALLOW_ALL = False
+
+CORS_ORIGIN_WHITELIST = (
+    'http//:localhost:8000',
+)
 
 ROOT_URLCONF = 'TimShop.urls'
 
