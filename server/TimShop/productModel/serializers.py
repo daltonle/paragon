2559a6c9175ba26dc4af72ef446ad451f61a4a1e
zeotuan <zeotuan@gmail.com>
2019-05-10 19:12:43 +1000

    update modelSuppserializer so that user can choose existing supplier and model to create new modelSupp

--- server/TimShop/productModel/serializers.py
+++ server/TimShop/productModel/serializers.py
@@ -16,11 +16,14 @@ class PModelSerializer(serializers.ModelSerializer):
 
 
 class ModelSuppSerializer(serializers.ModelSerializer):
-    supplier = SupplierSerializer(many=False)
-    pModel = PModelSerializer(many=False)
+    pModel = PModelSerializer(many=False, read_only=True)
+    pModel_id = serializers.PrimaryKeyRelatedField(queryset=PModel.objects.all(), source='pModel', write_only=True, required=False)
+    supplier = SupplierSerializer(many=False, read_only=True)
+    supplier_id = serializers.PrimaryKeyRelatedField( queryset= Supplier.objects.all(), source='supplier',
+                                                      write_only=True, required=False)
 
     class Meta:
         model = ModelSupp
-        fields = ('supplier','pModel','price','availability')
+        fields = ('supplier','supplier_id','pModel','pModel_id','price','availability')
 
 
