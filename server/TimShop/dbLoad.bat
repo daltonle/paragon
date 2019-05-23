ae0fbe96b6e23dd249c9643849d0fa42e29d4d66
Dalton <duong.daltonle@gmail.com>
2019-05-23 22:15:55 +1000

    temp changes

--- server/TimShop/dbLoad.bat
+++
@@ -1,9 +0,0 @@
-cd script
-py dbDelete.py
-
-cd ../
-py manage.py loaddata fixtures\account.json
-py manage.py loaddata fixtures\customers.json
-py manage.py loaddata fixtures\locations.json
-py manage.py loaddata fixtures\productModel.json
-py manage.py loaddata fixtures\sale.json
