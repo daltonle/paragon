c9f83e9ed300a0f067aa5f4c69b294abebc3cc5e
Dalton <duong.daltonle@gmail.com>
2019-05-24 00:40:10 +1000

    Persist user state in localStorage

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,9 @@
 import React, { Component } from "react"
 import { Provider } from "react-redux"
 import { Switch, Route } from "react-router-dom"
+import { PersistGate } from "redux-persist/integration/react"
 import "./App.scss"
-import store from "./state/store"
+import store, { persistor } from "./state/store"
 import CustomersPage from "./views/pages/customers/CustomersPage"
 import SalesPage from "./views/pages/sales/SalesPage"
 import LoginPage from "./views/pages/login/LoginPage"
@@ -11,13 +12,15 @@ class App extends Component {
   render() {
     return (
       <Provider store={store}>
-        <div className="App">
-          <Switch>
-            <Route path='/login' render={ props => <LoginPage {...props}/> } />
-            <Route path='/customers' render={ props => <CustomersPage {...props}/> } />
-            <Route path={['/', '/sales']} render={ props => <SalesPage {...props}/> } />
-          </Switch>
-        </div>
+        <PersistGate loading={null} persistor={persistor}>
+          <div className="App">
+            <Switch>
+              <Route path='/login' render={ props => <LoginPage {...props}/> } />
+              <Route path='/customers' render={ props => <CustomersPage {...props}/> } />
+              <Route path={['/', '/sales']} render={ props => <SalesPage {...props}/> } />
+            </Switch>
+          </div>
+        </PersistGate>
       </Provider>
     )
   }
--- client/src/state/ducks/index.js
+++ client/src/state/ducks/index.js
@@ -1,3 +1,2 @@
 export { default as customers } from './customers/reducers'
 export { default as sales } from './sales/reducers'
-export { default as user } from './user/reducers'
