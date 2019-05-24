e0c16dac241cca1868320bca5d846da564a36537
Dalton <duong.daltonle@gmail.com>
2019-05-25 06:36:25 +1000

    Add CRUD action for locations

--- client/src/App.js
+++ client/src/App.js
@@ -8,6 +8,7 @@ import CustomersPage from "./views/pages/customers/CustomersPage"
 import SalesPage from "./views/pages/sales/SalesPage"
 import LoginPage from "./views/pages/login/LoginPage"
 import ModelPage from "./views/pages/models/ModelPage"
+import LocationPage from "./views/pages/locations/LocationPage"
 
 class App extends Component {
   render() {
@@ -16,10 +17,11 @@ class App extends Component {
         <PersistGate loading={null} persistor={persistor}>
           <div className="App">
             <Switch>
-              <Route path='/login' render={ props => <LoginPage {...props}/> } />
-              <Route path='/customers' render={ props => <CustomersPage {...props}/> } />
-              <Route exact path={['/', '/sales']} render={ props => <SalesPage {...props}/> } />
-              <Route path='/models' render={ props => <ModelPage {...props}/> } />
+              <Route path='/login' render={props => <LoginPage {...props}/>} />
+              <Route path='/customers' render={props => <CustomersPage {...props}/>} />
+              <Route exact path={['/', '/sales']} render={props => <SalesPage {...props}/>} />
+              <Route path='/models' render={props => <ModelPage {...props}/>} />
+              <Route path='/locations' render={props => <LocationPage {...props} />} />
             </Switch>
           </div>
         </PersistGate>
--- client/src/views/components/appbar/Appbar.js
+++ client/src/views/components/appbar/Appbar.js
@@ -18,29 +18,15 @@ export const Appbar = (props) => (
     <Link to="/sales" className={classNames({[styles.menuActive]: props.active === 'sales', [styles.section]: true})}>Sales</Link>
     <Link to="/customers" className={classNames({[styles.menuActive]: props.active === 'customers', [styles.section]: true})}>Customers</Link>
     <Link to="/models" className={classNames({[styles.menuActive]: props.active === 'models', [styles.section]: true})}>Models</Link>
-    <div className={styles.section}>
-      Supplier
-    </div>
-    <div className={styles.section}>
-      Locations
-    </div>
-    <div className={styles.header}>
-      
-    </div>
-    <div className={styles.section}>
-      Groups
-    </div>
-    <div className={styles.section}>
-      Users
-    </div>
     <div className={styles.header}>
     </div>
-    <div className={styles.section}>
-      Stock order
+    <Link to="/users" className={classNames({[styles.menuActive]: props.active === 'users', [styles.section]: true})}>Users</Link>
+    <Link to="/locations" className={classNames({[styles.menuActive]: props.active === 'locations', [styles.section]: true})}>Locations</Link>
+    <div className={styles.header}>
     </div>
-    <div className={styles.section}>
-      Catalogue
-    </div>
+    <Link to="/suppliers" className={classNames({[styles.menuActive]: props.active === 'suppliers', [styles.section]: true})}>Suppliers</Link>
+    <Link to="/catalogues" className={classNames({[styles.menuActive]: props.active === 'catalogues', [styles.section]: true})}>Catalogues</Link>
+    <Link to="/stock-order" className={classNames({[styles.menuActive]: props.active === 'stockOrders', [styles.section]: true})}>Stock order</Link>
     <div className={styles.logout} onClick={() => {
       localStorage.removeItem('ParagonToken')
       props.history.push('/login')
