58fa37f5bf14bfec4dda44330b143f60a1b1871b
zeotuan <zeotuan@gmail.com>
2019-05-26 02:02:01 +1000

    refactor long method + duplicated code from def update in customeer serializer

--- server/TimShop/account/permissions.py
+++ server/TimShop/account/permissions.py
@@ -16,6 +16,11 @@ class IsSuperUser(permissions.BasePermission):
     def has_object_permission(self, request, view, obj):
         return request.user and (request.user.is_superuser or is_admin(request))
 
+def is_admin(request):
+    if hasattr(request.user, 'profile'):
+        return request.user.profile.group == "Admin"
+    return False
+
 
 class IsTokenValid(permissions.BasePermission):
     def has_permission(self, request, view):
@@ -29,8 +34,3 @@ class IsTokenValid(permissions.BasePermission):
         except BlackListedToken.DoesNotExist:
             is_allowed_user = True
         return is_allowed_user
-
-def is_admin(request):
-    if hasattr(request.user, 'profile'):
-        return request.user.profile.group == "Admin"
-    return False
