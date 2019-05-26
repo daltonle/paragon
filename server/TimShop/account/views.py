a833bb89a728f3f5a56ebbe3583613fa243997fa
zeotuan <zeotuan@gmail.com>
2019-05-26 15:59:20 +1000

    fix update not working properly

--- server/TimShop/account/views.py
+++ server/TimShop/account/views.py
@@ -58,27 +58,9 @@ class ChangePasswordView(UpdateAPIView):
     An endpoint for changing password.
     """
     serializer_class = ChangePasswordSerializer
-    model = User
-    permission_classes = [IsAuthenticated,]
-    
-    def get_object(self, queryset=None):
-        obj = self.request.user
-        return obj
+    queryset = User.objects.all()
+    permission_classes = [IsLoggedInUserOrAdmin,]  # only superuser or owner of the account can change password
 
-    def update(self, request, *args, **kwargs):
-        self.object = self.get_object()
-        serializer = self.get_serializer(data=request.data)
-
-        if serializer.is_valid():
-            # Check old password
-            if not self.object.check_password(serializer.data.get("old_password")):
-                return Response({"old_password": ["Wrong password."]}, status=HTTP_400_BAD_REQUEST)
-            # set_password also hashes the password that the user will get
-            self.object.set_password(serializer.data.get("new_password"))
-            self.object.save()
-            return Response("Success.", status=HTTP_200_OK)
-
-        return Response(serializer.errors, status=HTTP_400_BAD_REQUEST)
 
 class UserViewSet(viewsets.ModelViewSet):
     queryset = User.objects.all()
